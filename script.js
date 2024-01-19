9bb7e58dfc608201d7e9c4f5b8b8796482209229
Chenxi <lin.chenxi14@gmail.com>
2024-01-19 11:44:44 -0500

    styled the mobile design

--- script.js
+++ script.js
@@ -16,19 +16,25 @@ function renderSum() {
         let divAline = document.createElement('div');
         let image = document.createElement('img');
         let p = document.createElement('p');
-        let pCategory = document.createElement('p')
+        let pCategory = document.createElement('p');
+        let span = document.createElement('span')
         image.src = sum.icon;
         image.alt = 'icon';
         pCategory.textContent = sum.category
         divCategory.className = sum.category;
         divAline.className = 'aline';
-        p.textContent = `${sum.score} / 100`
-
+        p.className = 'decriptionP';
+        span.className = 'sumOutcome';
+        span.textContent = sum.score;
+        p.textContent = ' / 100';
+        
         scoreContainer.appendChild(divCategory);
         divCategory.appendChild(divAline);
         divAline.appendChild(image);
         divAline.appendChild(pCategory)
         divCategory.appendChild(p);
+        p.appendChild(span)
+        p.insertBefore(span, p.firstChild)
     })
 }
 
@@ -41,6 +47,6 @@ function averageScore() {
     });
     let totalAmount = Math.floor(total / summary.length);
     avgTotal.textContent = totalAmount;
-    avgScore.textContent = totalAmount;
+    avgScore.textContent = '65%';
 }
 
